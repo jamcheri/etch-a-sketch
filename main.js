fa13ca6492b4ec847bf77278319a5085a57397c3
jamcheri <james.d.cherian@gmail.com>
2021-10-21 14:43:01 -0700

    working prototype with hover

--- main.js
+++ main.js
@@ -3,8 +3,17 @@
 for (let i=0; i<16; i++) {
     const container = document.querySelector("#container"); //select container HTML element
     const gridDiv = document.createElement("div");          //create div HTML element
-    gridDiv.classList.add("grid");  
-    gridDiv.textContent = "Hello World!";                           //add class "grid" to element so that it can be manipulated with css
+    gridDiv.classList.add("grid");                          //add class "grid" to element so that it can be manipulated with css
+    gridDiv.textContent = "Hello World!";  
+
+    gridDiv.addEventListener("mouseover", () => {
+        gridDiv.classList.add("hover")
+    });
+
+    gridDiv.addEventListener("mouseout", () => {
+        gridDiv.classList.remove("hover")
+    });
+
     container.appendChild(gridDiv);
 }
 
