b7485690672903b1b246326ca5c3a676f5206d33
jamcheri <james.d.cherian@gmail.com>
2021-10-21 23:00:37 -0700

    finished working prototype with reset

--- main.js
+++ main.js
@@ -1,20 +1,60 @@
+let userInput = 16;
 
-//create 16 div elements
-for (let i=0; i<16; i++) {
+//function that make a grid of dimension gridSide x gridSide
+function makeGrid(gridSide) {
+    gridSide = userInput;
+    let divNum = gridSide * gridSide;  //number of grid divs to make
+    let minWidth = 100/gridSide;        //css style parameters
+    let minHeight = 100/gridSide;       
+
     const container = document.querySelector("#container"); //select container HTML element
-    const gridDiv = document.createElement("div");          //create div HTML element
-    gridDiv.classList.add("grid");                          //add class "grid" to element so that it can be manipulated with css
-    gridDiv.textContent = "Hello World!";  
 
-    gridDiv.addEventListener("mouseover", () => {
-        gridDiv.classList.add("hover")
-    });
+    //checks if there are already children in container, if so deletes them.
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 
-    gridDiv.addEventListener("mouseout", () => {
-        gridDiv.classList.remove("hover")
-    });
-
-    container.appendChild(gridDiv);
+    //loop to create grid items
+    for (i=0; i<divNum; i++) {
+        const gridDiv = document.createElement("div");  //create div HTML element
+        gridDiv.classList.add("grid");                  //add class "grid" to element so that it can be manipulated with css
+        gridDiv.style.minWidth = minWidth + "%";        //set div height equal to 1/gridSide. 
+        gridDiv.style.minHeight = minHeight + "%";
+    
+        gridDiv.addEventListener("mouseover", () => {   //event listener for mouseover
+            gridDiv.classList.add("hover")
+        });
+    
+        /*gridDiv.addEventListener("mouseout", () => {  //no mouseout as this is etch-a-sketch
+            gridDiv.classList.remove("hover")
+        });*/   
+    
+        container.appendChild(gridDiv);                 //attach div to container
+    }    
 }
 
+const button = document.querySelector("#button");       //button to reset 
+button.addEventListener('click', () => {
+    userInput = prompt("What dimensions should the new grid be? X number by X number:");
 
+    while (userInput > 100) {                           //don't let grid get larger than 100x100
+        userInput = prompt("Grid dimension cannot be larger than 100, please enter a lower dimension:")
+    }
+
+    makeGrid(userInput);
+});
+
+makeGrid();     //run main function on startup
+
+
+// could also use gridDiv.setAttribute("style")
+
+//could also use 
+//gridDiv.style.cssText = "min-width = " + gridSide; 
+//gridDiv.style.cssText = "min-width = " + gridSide;
+
+//could also use this
+//document.getElementById('grid').style.gridDiv.
+
+
+//button on 1. resets the grid and 2. prompts to ask how many grid items to make
